3852c440e7fecde8c690740829d7c09bb28f3138
Ryan Lembo-Ehms <rjl7448@g.rit.edu>
2026-04-16 16:21:40 -0400

    update sys_read to latest snapshot

--- kernel/syscalls.c
+++ kernel/syscalls.c
@@ -501,6 +501,9 @@ SYSIMPL(exec)
 	pcb->context = stk_setup( stk, where, args );
 	assert1( pcb->context != NULL );
 
+  // put the new stack in place
+  pcb->stack = stk;
+
 	// now we can safely free the old stack
 	stk_free( oldstack );
 
@@ -579,7 +582,7 @@ SYSIMPL(read) {
 		if( n < 1 ) {
 			// nothing available, so we'll block
 			pcb->state = STATE_BLOCKED;
-			assert1( que_insert(sioread,(void *)pcb) != E_SUCCESS );
+			assert1( que_insert(sioread,(void *)pcb) == E_SUCCESS );
 			// dispatch a new process
 			dispatch();
 			SYSCALL_EXIT(0);
